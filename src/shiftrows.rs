a1c58d4be4ff32742f742954bd2ebc544cb51de8
Kushal <kush.al@outlook.com>
2025-12-02 16:10:40 +0000

    refactor to lib and no_std + alloc

--- src/shiftrows.rs
+++ src/shiftrows.rs
@@ -1,25 +1,21 @@
+use crate::State;
 
-pub fn shift_rows(state:Vec<Vec<u8>>)->Vec<Vec<u8>>{
-    let mut shifted_state = vec![vec![0;4];4];
-
-    for (c,column) in shifted_state.iter_mut().enumerate(){
-        for (r,row) in column.iter_mut().enumerate(){
-            *row = state[(c+r)%4][r];
+pub(crate) fn shift_rows(state: State) -> State {
+    let mut shifted = [[0u8; 4]; 4];
+    for c in 0..4 {
+        for r in 0..4 {
+            shifted[c][r] = state[(c + r) % 4][r];
         }
     }
-    return shifted_state;
-
+    shifted
 }
 
-pub fn inv_shift_rows(state:Vec<Vec<u8>>)->Vec<Vec<u8>>{
-    let mut shifted_state = vec![vec![0;4];4];
-
-    for (c,column) in shifted_state.iter_mut().enumerate(){
-        for (r,row) in column.iter_mut().enumerate(){
-            *row = state[(4+c-r)%4][r];
+pub(crate) fn inv_shift_rows(state: State) -> State {
+    let mut shifted = [[0u8; 4]; 4];
+    for c in 0..4 {
+        for r in 0..4 {
+            shifted[c][r] = state[(4 + c - r) % 4][r];
         }
     }
-
-    return shifted_state;
-
+    shifted
 }
